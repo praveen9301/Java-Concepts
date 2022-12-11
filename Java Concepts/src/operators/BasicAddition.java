87e14eb36f597439523015b9635dd494b7bb8b97
prj30 <prj30@HP-PraveenRaj>
2022-12-11 11:34:50 +0530

    Modified the Basic Operator Class.

--- Java Concepts/src/operators/BasicAddition.java
+++ Java Concepts/src/operators/BasicAddition.java
@@ -3,8 +3,15 @@ package operators;
 public class BasicAddition {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Values");
+
+		/*
+		 * Approach 1: Directly we can perform operation using addition operator (+) in
+		 * the print statement.
+		 */
+		System.out.println(20+30);
+
+		int value1 = 20;
+		int value2 = 30;
 
 	}
 
