18d8b490f0c19bca2d443348973eefbe03d7ca3b
PraveenRaj <praveen9301@github>
2022-12-11 11:45:53 +0530

    Modified 2: Basic Addition | 2nd Approach added

--- Java Concepts/src/operators/BasicAddition.java
+++ Java Concepts/src/operators/BasicAddition.java
@@ -8,10 +8,19 @@ public class BasicAddition {
 		 * Approach 1: Directly we can perform operation using addition operator (+) in
 		 * the print statement.
 		 */
-		System.out.println(20+30);
+		System.out.println(20 + 30);
 
 		int value1 = 20;
 		int value2 = 30;
+
+		/*
+		 * Approach 2: Perform the addition Operation and assign to a variable. Print
+		 * the Statement
+		 */
+		int sumOftheValues = value1 + value2;
+		System.out.println("Result: " + sumOftheValues);
+		
+		/**/
 
 	}
 
